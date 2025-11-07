7668f9eb3e5842b82d4b1f0d80db4c5166138503
robert-117 <rcarp2010@gmail.com>
2025-11-07 13:36:01 -0800

    add functionality to toggle case sensitive characters

--- count_strings.py
+++ count_strings.py
@@ -2,30 +2,41 @@ import argparse
 
 argparser = argparse.ArgumentParser()
 
-argparser.add_argument('file')
-argparser.add_argument('word', type=str)
+argparser.add_argument('file', help='designated file to search in')
+argparser.add_argument('word', help='search for word in file',type=str)
+argparser.add_argument('-s', '--case-sensitive', help='enables case sensitivity when searching', action='store_false')
 argparser.add_argument('-v', '--verbose', action="store_true")
 args = argparser.parse_args()
 
 file_name = args.file
 word = args.word
 verbose = args.verbose
+case = args.case_sensitive
 
-def open_file(file_name, word):
+def search_file(file_name, word):
     w = 0
-    word = word.lower()
-    with open(file_name, 'r') as file:
-        for line in file:
-            line = line.strip().lower()
-            if line == word:
-                w += 1
+    if case is True:
+        word = word.lower()
+        with open(file_name, 'r') as file:
+            for line in file:
+                line = line.strip().lower()
+                if line == word:
+                    w += 1
+    else:
+        with open(file_name, 'r') as file:
+            for line in file:
+                line = line.strip()
+                if line == word:
+                    w += 1
     return w
 
-answer = open_file(file_name, word)
+answer = search_file(file_name, word)
 
 if answer == 0:
     print(f"'{word}' was not found in {file_name}")
 elif verbose:
     print(f"there are {answer} occurrences of {word} in {file_name}")
+elif verbose & case: #TODO: fix output message
+    print(f"there are {answer} occurrences of {word} in {file_name} with case sensitivity")
 else:
     print(answer)

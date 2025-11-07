5f837bd7ce60461e756457d7249cde71ee3fe8a8
robert-117 <rcarp2010@gmail.com>
2025-11-07 13:04:56 -0800

    updated changes for count_strings.py for verbosity and logic

--- count_strings.py
+++ count_strings.py
@@ -1,23 +1,30 @@
 import argparse
-import os
-from os import close
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('file')
 argparser.add_argument('word', type=str)
+argparser.add_argument('-v', '--verbose', action="store_true")
 args = argparser.parse_args()
 
 file_name = args.file
 word = args.word
+verbose = args.verbose
 
 def open_file(file_name, word):
     w = 0
-    with open(file_name) as f:
-        for line in f:
+    with open(file_name, 'r') as file:
+        for line in file:
+            line = line.strip()
             if line == word:
                 w += 1
-    f.close()
     return w
 
-print(open_file(file_name, word))
+answer = open_file(file_name, word)
+
+if answer == 0:
+    print(f"'{word}' was not found in {file_name}")
+elif verbose:
+    print(f"there are {answer} occurrences of {word} in {file_name}")
+else:
+    print(answer)

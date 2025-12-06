2f3a001e1a3826ac14768f98bfaa1376a54310bd
robert-117 <rcarp2010@gmail.com>
2025-12-06 13:39:35 -0800

    added arg parsing to calculate_expenses.py

--- modifying_csv/calculate_expenses.py
+++ modifying_csv/calculate_expenses.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import argparse
 
-df = pd.read_csv('expenses.csv')
+argparser = argparse.ArgumentParser()
+argparser.add_argument("file", type=str, help="specify csv file")
+argparser.add_argument("column_name", type=str, help="specify column name file")
+args = argparser.parse_args()
 
+df = pd.read_csv(args.file)
+column_name = args.column_name
+# df = pd.read_csv('expenses.csv')
 # pd.set_option('display.max_columns', 100)
 # pd.set_option('display.max_rows', 100)
 
-total_amount = df['amount'].sum()
-max_amount = df['amount'].max()
+total_amount = round(df[column_name].sum(), 2)
+max_amount = round(df[column_name].max(), 2)
 
 print(f'Total amount:{total_amount}')
 print(f'Highest amount:{max_amount}')

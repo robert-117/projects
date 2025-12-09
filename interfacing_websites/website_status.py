c5d05c220801a246f65c10951029059356664078
robert-117 <rcarp2010@gmail.com>
2025-12-08 16:43:28 -0800

    created new website status checker script

---
+++ interfacing_websites/website_status.py
@@ -0,0 +1,21 @@
+from email.contentmanager import maintype
+
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', action='extend', nargs='+', help='check status codes for websites')
+
+args = parser.parse_args()
+url = args.url
+
+def fetch_status(url):
+    for website in url:
+        try:
+            response = requests.get(website)
+        except requests.HTTPError as e:
+            print(e)
+        finally:
+            print(f'status code {response.status_code}: {website}')
+
+fetch_status(url)

a1324590aa9f7a09699a6bf537d781b1ad276e7c
robert-117 <rcarp2010@gmail.com>
2026-01-06 13:05:01 -0800

    updating total line count + response codes found in logs

--- log_analyzer/mini_log_summary.py
+++ log_analyzer/mini_log_summary.py
@@ -14,10 +14,11 @@ output timeline.txt with
 - first/last timestamp
 - a short list of notable events (WARN/ERROR, sorted by time)
 '''
-import json, argparse, datetime, re
+import json, argparse, datetime, re, collections
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
+parser.add_argument("-p", "--print", help="Print to stdout", action="store_true")
 args = parser.parse_args()
 
 def format_detector():
@@ -34,7 +35,7 @@ def format_detector():
 def main():
    if format_detector() == "json":
        json_parser()
-   elif format_detector() == "syslog": # perform logic from
+   elif format_detector() == "syslog":
        syslog_parser()
    else:
        print(format_detector())
@@ -44,11 +45,30 @@ def syslog_parser():
 
 def json_parser():
     with open(args.file) as f:
+        counter = 0
+        level_list = []
         for line in f:
             data = json.loads(line)
+            counter += 1
+            level_list.append(data["level"])
+            if args.print is True:
+                print(data)
+        level_count = collections.Counter(level_list)
+
+        print(f"Number of lines found in file: {counter} lines")
+        print(f"Found the following number of response codes:")
+        for k, v in level_count.items():
+            print(f"{k}: {v}")
+
         return data
 
-
+def json_summary(data):
+    pass
+    # counter = 0
+    # level_list = []
+    # for line in data:
+    #     counter += 1
+    #     level_list.append(data["level"])
 
 if __name__ == "__main__":
     main()
